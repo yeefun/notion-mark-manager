cae7d18e2a59b60250d2326a0d706626cac335d2
yeefun <szyeefun@gmail.com>
2020-08-22 15:57:31 +0800

    refactor: use shared ga methods

--- src/js/options.js
+++ src/js/options.js
@@ -1,29 +1,7 @@
-// https://davidsimpson.me/2014/05/27/add-googles-universal-analytics-tracking-chrome-extension/
-(function (i, s, o, g, r, a, m) {
-  i['GoogleAnalyticsObject'] = r;
-  (i[r] =
-    i[r] ||
-    function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(
-  window,
-  document,
-  'script',
-  'https://www.google-analytics.com/analytics.js',
-  'ga'
-);
+import { loadGa, sendPageview } from './utils/ga.js';
 
-window.ga('create', 'UA-134635576-1', 'auto');
-// Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
-window.ga('set', 'checkProtocolTask', function () {});
-window.ga('require', 'displayfeatures');
-window.ga('send', 'pageview', '/options.html');
+loadGa();
+sendPageview('/options.html');
 
 let originColors = [];
 let originTab = '';
