38d49f94bc419a9463cc257fe0d22d87f56aa0af
yeefun <szyeefun@gmail.com>
2020-08-22 15:32:35 +0800

    refactor(background): improve readability

--- src/js/background.js
+++ src/js/background.js
@@ -1,17 +1,25 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+chrome.runtime.onInstalled.addListener(handleInstalled);
+
+function handleInstalled() {
+  {
+    // If `ruleIds` is `undefined`, all registered rules of this extension are removed.
+    let ruleIds = undefined;
+    chrome.declarativeContent.onPageChanged.removeRules(
+      ruleIds,
+      callbackWhenRulesWereRemoved
+    );
+  }
+
+  function callbackWhenRulesWereRemoved() {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-              hostEquals: 'www.notion.so',
-              schemes: ['https'],
-            },
+            pageUrl: { hostEquals: 'www.notion.so' },
           }),
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()],
       },
     ]);
-  });
-});
+  }
+}
