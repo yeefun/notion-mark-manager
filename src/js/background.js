74ba463574987d85fb54c5bdd0570464b17d4cc0
yeefun <szyeefun@gmail.com>
2020-09-12 17:02:49 +0800

    feat: support Firefox

--- src/js/background.js
+++ src/js/background.js
@@ -1,25 +1,30 @@
-chrome.runtime.onInstalled.addListener(handleInstalled);
-
-function handleInstalled() {
-  {
-    // If `ruleIds` is `undefined`, all registered rules of this extension are removed.
-    let ruleIds = undefined;
-    chrome.declarativeContent.onPageChanged.removeRules(
-      ruleIds,
-      callbackWhenRulesWereRemoved
-    );
+(function init() {
+  // Firefox doesn't support `declarativeContent`.
+  if (!chrome.declarativeContent) {
+    return;
   }
 
-  function callbackWhenRulesWereRemoved() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'www.notion.so' },
-          }),
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  }
-}
+  chrome.runtime.onInstalled.addListener(function handleInstalled() {
+    {
+      // If `ruleIds` is `undefined`, all registered rules of this extension are removed.
+      const ruleIds = undefined;
+      chrome.declarativeContent.onPageChanged.removeRules(
+        ruleIds,
+        callbackWhenRulesWereRemoved
+      );
+    }
+
+    function callbackWhenRulesWereRemoved() {
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { hostEquals: 'www.notion.so' },
+            }),
+          ],
+          actions: [new chrome.declarativeContent.ShowPageAction()],
+        },
+      ]);
+    }
+  });
+})();
